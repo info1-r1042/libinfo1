badd57c59b5d0ac8ba0c8da92f9d5d114f137899
Javier Balloffet <javier.balloffet@gmail.com>
2020-04-27 21:08:10 -0300

    Refactor socket functions

--- include/info1.h
+++ include/info1.h
@@ -7,20 +7,22 @@
 #ifndef INFO1_H_
 #define INFO1_H_
 
+#include <stdio.h>
+
 enum {
   EXITO = 0,
   ERROR
 };
 
-typedef char* string;
+// Funciones para manejo de Entradas.
 
-// Entradas
+typedef char* string;
 
 /*!
  * @brief Obtiene un char desde stdin.
  *
  * @param[in] mensaje Mensaje de solicitud de ingreso de datos.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
 char obtener_char(string mensaje);
 
@@ -28,7 +30,7 @@ char obtener_char(string mensaje);
  * @brief Obtiene un int desde stdin.
  *
  * @param[in] mensaje Mensaje de solicitud de ingreso de datos.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
 int obtener_int(string mensaje);
 
@@ -36,7 +38,7 @@ int obtener_int(string mensaje);
  * @brief Obtiene un long desde stdin.
  *
  * @param[in] mensaje Mensaje de solicitud de ingreso de datos.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
 long obtener_long(string mensaje);
 
@@ -44,7 +46,7 @@ long obtener_long(string mensaje);
  * @brief Obtiene un float desde stdin.
  *
  * @param[in] mensaje Mensaje de solicitud de ingreso de datos.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
 float obtener_float(string mensaje);
 
@@ -52,7 +54,7 @@ float obtener_float(string mensaje);
  * @brief Obtiene un double desde stdin.
  *
  * @param[in] mensaje Mensaje de solicitud de ingreso de datos.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
 double obtener_double(string mensaje);
 
@@ -60,157 +62,156 @@ double obtener_double(string mensaje);
  * @brief Obtiene un string desde stdin.
  *
  * @param[in] mensaje Mensaje de solicitud de ingreso de datos.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
 string obtener_string(string mensaje);
 
-// Archivos
+// Funciones para manejo de Archivos.
 
 /*!
  * @brief Abre un archivo en modo lectura/escritura.
  *
  * @param[in] archivo Nombre del archivo.
- * @return	  EXITO en caso de haber podido abrir el archivo correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber podido abrir el archivo correctamente, o ERROR en caso contrario.
  */
-int abrir_archivo(char* archivo);
+file abrir_archivo(char* archivo);
 
 /*!
  * @brief Lee una línea del archivo.
  *
  * @param[out] var1 Puntero al arreglo de calificaciones.
- * @return	   EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return     EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
-int leer_linea(char* linea);
+int leer_linea(file archivo, char* linea);
 
 /*!
  * @brief Escribe una línea en el archivo.
  *
  * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @return    EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
  */
-int escribir_linea(char* linea);
+int escribir_linea(file archivo, char* linea);
 
 /*!
  * @brief Ubica el cursor al comienzo del archivo.
  */
-void resetear_cursor();
+void resetear_cursor(file archivo);
 
 /*!
  * @brief Cierra el archivo.
  */
-void cerrar_archivo();
+void cerrar_archivo(file archivo);
 
-// Sockets
+// Funciones para manejo de Sockets.
 
-// Servidor
+typedef FILE* file;
+
+// 1. TCP - Protocolo orientado a la conexión.
+
+// 1.1. Lado servidor.
 
 /*!
- * @brief Abre un socket UNIX.
+ * @brief Abre un socket INET TCP como servidor.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in] int Puerto donde se desea abrir el socket.
+ * @return    Descriptor del socket servidor en caso de éxito, o NULL en caso contrario.
  */
-int abrir_socket_unix(char* servicio);
+int abrir_socket_servidor_tcp(int puerto);
 
 /*!
- * @brief Abre un socket INET.
+ * @brief Acepta una conexión INET TCP entrante desde un cliente.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in] int Descriptor del socket servidor.
+ * @return    Descriptor del socket cliente en caso de éxito, o NULL en caso contrario.
  */
-int abrir_socket_inet(char* servicio);
+int aceptar_conexion_entrante_tcp(int socket);
+
+// 1.2. Lado cliente.
 
 /*!
- * @brief Abre un socket UDP.
+ * @brief Abre un socket INET TCP como cliente.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in] ip Direccion IP a la que se quiere conectar.
+ * @param[in] puerto Puerto al que se quiere conectar.
+ * @return    Descriptor del socket en caso de éxito, o NULL en caso contrario.
  */
-int abrir_socket_udp(char* servicio);
+int abrir_socket_cliente_tcp(char* ip, int puerto);
+
+// 1.3. Ambos lados.
 
 /*!
- * @brief Acepta una conexión entrante desde un cliente.
+ * @brief Lee datos desde un socket INET TCP.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in]  socket Descriptor del socket.
+ * @param[out] buffer Buffer donde almacenar los datos recibidos.
+ * @param[in]  largo Largo del buffer en bytes.
+ * @return     Cantidad de bytes leídos, o ERROR en caso contrario.
  */
-int aceptar_conexion_cliente(int descriptor);
-
-// Cliente
+int recibir_datos_tcp(int socket, char* buffer, int largo);
 
 /*!
- * @brief Abre una conexión UNIX con un servidor.
+ * @brief Escribe datos en un socket INET TCP.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in]  socket Descriptor del socket.
+ * @param[out] buffer Buffer donde están almacenados los datos a enviar.
+ * @param[in]  largo Cantidad de bytes a enviar.
+ * @return     Cantidad de bytes enviados, o ERROR en caso contrario.
  */
-int abrir_conexion_unix(char* servicio);
+int enviar_datos_tcp(int socket, char* buffer, int largo);
 
 /*!
- * @brief Abre una conexión INET con un servidor.
+ * @brief Cierra un socket INET TCP.
+ */
+void cerrar_socket_tcp(int socket);
+
+// 2. UDP - Protocolo orientado al envío de datos.
+
+// 2.1. Lado servidor.
+
+/*!
+ * @brief Abre un socket INET UDP como servidor.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in] int Puerto donde se desea abrir el socket.
+ * @return    Descriptor del socket servidor en caso de éxito, o NULL en caso contrario.
  */
-int abrir_conexion_inet(char* servicio);
+int abrir_socket_servidor_udp(int puerto);
+
+// 2.2. Lado cliente.
 
 /*!
- * @brief Abre una conexión UDP con un servidor.
+ * @brief Abre un socket INET UDP como cliente.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in] ip Direccion IP a la que se quiere conectar.
+ * @param[in] puerto Puerto al que se quiere conectar.
+ * @return    Descriptor del socket en caso de éxito, o NULL en caso contrario.
  */
-int abrir_conexion_udp();
+int abrir_socket_cliente_udp(char* ip, int puerto);
 
-// Comunes
+// 2.3. Ambos lados.
 
 /*!
- * @brief Lee datos desde un socket.
+ * @brief Lee datos desde un socket INET UDP.
  *
- * @param[in]  var1 Puntero al arreglo de calificaciones.
- * @param[out] var1 Puntero al arreglo de calificaciones.
- * @param[in]  var1 Puntero al arreglo de calificaciones.
- * @return	   EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in]  socket Descriptor del socket.
+ * @param[out] buffer Buffer donde almacenar los datos recibidos.
+ * @param[in]  largo Largo del buffer en bytes.
+ * @return     Cantidad de bytes leídos, o ERROR en caso contrario.
  */
-int leer_socket(int socket, char* buffer, int bytes_a_leer);
+int recibir_datos_udp(int socket, char* buffer, int largo);
 
 /*!
- * @brief Escribe datos en un socket.
+ * @brief Escribe datos en un socket INET UDP.
  *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @param[in]  socket Descriptor del socket.
+ * @param[out] buffer Buffer donde están almacenados los datos a enviar.
+ * @param[in]  largo Cantidad de bytes a enviar.
+ * @return     Cantidad de bytes enviados, o ERROR en caso contrario.
  */
-int escribir_socket(int socket, char* buffer, int bytes_a_escribir);
+int enviar_datos_udp(int socket, char* buffer, int largo);
 
 /*!
- * @brief Lee datos desde un socket UDP.
- *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
+ * @brief Cierra un socket INET UDP.
  */
-int leer_socket_udp(int, struct sockaddr*, int*, char*, int);
-
-/*!
- * @brief Escribe datos en un socket UDP.
- *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
- */
-int escribir_socket_udp(int, struct sockaddr*, int, char*, int);
-
-/*!
- * @brief Obtiene la dirección de un socket UDP.
- *
- * @param[in] var1 Puntero al arreglo de calificaciones.
- * @return	  EXITO en caso de haber terminado la carga correctamente, o ERROR en caso contrario.
- */
-int obtener_direccion_udp(char*, char*, struct sockaddr*, int*);
-
-/*!
- * @brief Cierra un socket.
- */
-void cerrar_socket();
+void cerrar_socket_udp(int socket);
 
 #endif // INFO1_H_
