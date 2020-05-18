fbc7b28bfa44c47c544e3710ab0ffae5a9bf68af
Javier Balloffet <javier.balloffet@gmail.com>
2020-05-17 22:25:46 -0300

    Added setup and teardown functions

--- src/info1_entradas.c
+++ src/info1_entradas.c
@@ -4,4 +4,24 @@
  * @author Javier Balloffet <javier.balloffet@gmail.com>
  * @date   Apr 26, 2020
  */
-#include "info1.h"
+// #include "info1.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void setup() __attribute__((constructor));
+
+static void teardown();
+
+static void setup() {
+  printf("Setup\n");
+  atexit(teardown);
+}
+
+static void teardown() {
+  printf("Teardown\n");
+}
+
+int main(void) {
+  return 0;
+}
